54314b54f3f8cfa203e1571d070707700fd07eaf
ohmizy <omarjabr49@yahoo.com>
2022-10-13 09:47:29 -0700

    Initial commit

---
+++ src/test/java/com/ohmizy/daikichiroutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohmizy.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
